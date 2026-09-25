d6279f33f8f6b722401d8e2ab48f7103033e76f3
Akinori99 <akinori.work99@gmail.com>
2026-09-25 23:46:57 +0900

    feat: PhysicsEngineに重力処理を追加

--- src/physics/PhysicsEngine.ts
+++ src/physics/PhysicsEngine.ts
@@ -2,11 +2,20 @@ import type { Position } from '../character/model/Position';
 import type { Velocity } from '../character/model/Velocity';
 
 export class PhysicsEngine {
+  private readonly gravity: number;
+
+  constructor(gravity = 1800) {
+    this.gravity = gravity;
+  }
+
   applyGravity(
-    _position: Position,
-    _velocity: Velocity,
-    _deltaTime: number,
-  ): void {}
+    position: Position,
+    velocity: Velocity,
+    deltaTime: number,
+  ): void {
+    velocity.y += this.gravity * deltaTime;
+    position.y += velocity.y * deltaTime;
+  }
 
   isGrounded(
     _position: Position,
